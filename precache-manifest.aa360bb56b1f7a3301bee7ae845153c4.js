c43d0a1c89950c3167cf4554736a298ce76c9704
Kim hyunwoo <snltpdua@gmail.com>
2020-01-31 13:53:32 +0900

    Updates

---
+++ precache-manifest.aa360bb56b1f7a3301bee7ae845153c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a0650c6aeffb07a27d3575becc25fc",
+    "url": "/cartfolioTest/index.html"
+  },
+  {
+    "revision": "18580907ca9f30505c91",
+    "url": "/cartfolioTest/static/css/main.866e9b05.chunk.css"
+  },
+  {
+    "revision": "90a703fd9adda030f2ea",
+    "url": "/cartfolioTest/static/js/2.16586a46.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cartfolioTest/static/js/2.16586a46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18580907ca9f30505c91",
+    "url": "/cartfolioTest/static/js/main.7690ab35.chunk.js"
+  },
+  {
+    "revision": "3357e64cf4ce09211003",
+    "url": "/cartfolioTest/static/js/runtime-main.4ebbc21b.js"
+  },
+  {
+    "revision": "476867ea0e7a363965cbf74f3efe4ca9",
+    "url": "/cartfolioTest/static/media/image1.476867ea.jpg"
+  },
+  {
+    "revision": "f4b232a8242dc102677da690098ab0ef",
+    "url": "/cartfolioTest/static/media/image2.f4b232a8.jpg"
+  },
+  {
+    "revision": "84ff38561498da82d33fd7cd24bc1e1f",
+    "url": "/cartfolioTest/static/media/image3.84ff3856.jpg"
+  },
+  {
+    "revision": "8042bf13b222976ffac7bbcf50eab797",
+    "url": "/cartfolioTest/static/media/image4.8042bf13.jpg"
+  },
+  {
+    "revision": "89976761c1b21ce7daeafdcdd1ab14ad",
+    "url": "/cartfolioTest/static/media/image5.89976761.jpg"
+  }
+]);
